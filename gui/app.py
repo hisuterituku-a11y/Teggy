44244f2664f93a5765327c4299e2d400730dfcd0
hisuterituku-a11y <hisuterituku@gmail.com>
2026-07-25 15:21:53 +0500

    refactor: remove legacy modules

--- gui/app.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QApplication
-from gui.main_window import MainWindow
-
-
-class TegiApp(QApplication):
-    def __init__(self):
-        super().__init__([])
-        self.window = MainWindow()
-        self.window.show()
